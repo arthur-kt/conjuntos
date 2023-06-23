4ad374a1ca11b1d0ef90115cd72b10e17f6c99f0
Arthur Kafrouni Tomazi <arthur.k.t@hotmail.com>
2023-06-23 16:01:45 -0300

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,30 @@
+"""
+Conjuntos
+
+Descrição: Este programa apresenta o funicionamento dos conjuntos em python, com o intuito de aprendizado
+Autor:Arthur Kafrouni Tomazi
+Data: 23/06/2023
+Versão: 0.0.1
+"""
+
+# Atribuição de variáveis
+
+lista_1 = [8,1,2,3,3,3,4,7,9,11, 8,47,1]
+conjunto_1 = set(lista_1)
+
+conjunto_2 = {1,3,3,5,5,0,0,23,-2}
+
+# Entrada de dados
+
+
+# Processamento de dados
+
+conjunto_2.add(600)
+conjunto_2.remove(0)
+
+# Saída de dados
+
+print("Conjunto 1:",conjunto_1)
+
+print("Conjunto 2",conjunto_2)
+
